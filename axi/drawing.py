2d661939879479d3ad15fd848ed3932075ba0f0d
eliotkaplan <kaplan.eliotj@gmail.com>
2022-09-23 16:28:00 -0400

    opengl renderer

--- axi/drawing.py
+++ axi/drawing.py
@@ -6,8 +6,9 @@ from math import sin, cos, radians, hypot
 from typing import Optional
 
 import matplotlib
-from matplotlib import pyplot as plt, patches
-from matplotlib.path import Path as PltPath
+import numpy as np
+from shapely.geometry import MultiLineString, Polygon
+from shapely.ops import split
 
 from .paths import (
     simplify_paths,
@@ -20,10 +21,6 @@ from .paths import (
     Point,
     reloop_paths,
 )
-
-import numpy as np
-from shapely.geometry import MultiLineString, Polygon
-from shapely.ops import split
 
 matplotlib.use("TkAgg")
 
@@ -207,7 +204,7 @@ class Drawing(object):
         return Drawing(join_paths(self.paths, tolerance))
 
     def reloop_paths(
-        self, reverse: bool = False, rng: Optional[np.random.Generator] = None
+            self, reverse: bool = False, rng: Optional[np.random.Generator] = None
     ) -> Drawing:
         return Drawing(reloop_paths(self.paths, reverse, rng))
 
@@ -243,7 +240,8 @@ class Drawing(object):
         return self.transform(func)
 
     def rotate(
-        self, angle: float, anchor: tuple[float, float] = (0, 0), degrees: bool = False
+            self, angle: float, anchor: tuple[float, float] = (0, 0),
+            degrees: bool = False
     ) -> Drawing:
         d = self.translate(-anchor[0], -anchor[1])
         if degrees:
@@ -271,7 +269,7 @@ class Drawing(object):
         return self.move(width / 2, height / 2, 0.5, 0.5)
 
     def rotate_to_fit(
-        self, width: float, height: float, step: float = 0.05, degrees: bool = False
+            self, width: float, height: float, step: float = 0.05, degrees: bool = False
     ) -> Optional[Drawing]:
         if degrees:
             step = radians(step)
@@ -302,7 +300,7 @@ class Drawing(object):
 
     @staticmethod
     def multi_scale_to_fit(
-        drawings: list[Drawing], width: float, height: float, padding: float = 0
+            drawings: list[Drawing], width: float, height: float, padding: float = 0
     ) -> list[Drawing]:
         combined = Drawing.combine(drawings)
         combined_scaled = combined.scale_to_fit(width, height, padding)
@@ -312,22 +310,24 @@ class Drawing(object):
         scale_y = combined_scaled.height / combined.height
         combined_scaled_no_offset = combined.scale(scale_x, scale_y)
         offset_x = (
-            combined_scaled.bounds[0] - combined_scaled_no_offset.bounds[0] + padding
+                combined_scaled.bounds[0] - combined_scaled_no_offset.bounds[
+            0] + padding
         )
         offset_y = (
-            combined_scaled.bounds[1] - combined_scaled_no_offset.bounds[1] + padding
+                combined_scaled.bounds[1] - combined_scaled_no_offset.bounds[
+            1] + padding
         )
         return [
             d.scale(scale_x, scale_y).translate(offset_x, offset_y) for d in drawings
         ]
 
     def rotate_and_scale_to_fit(
-        self,
-        width: float,
-        height: float,
-        padding: float = 0,
-        step=0.01,
-        degrees: bool = False,
+            self,
+            width: float,
+            height: float,
+            padding: float = 0,
+            step=0.01,
+            degrees: bool = False,
     ) -> Drawing:
         if degrees:
             step = radians(step)
@@ -365,43 +365,3 @@ class Drawing(object):
                 new_path += tmp[1:]
             paths.append(new_path)
         return Drawing(paths)
-
-    @staticmethod
-    def render(layers: list[Drawing], width: float, height: float):
-        colors = [
-            "blue",
-            "red",
-            "orange",
-            "darkgreen",
-            "aqua",
-            "fuchsia",
-            "lime",
-            "hotpink",
-        ]
-        fig, ax = plt.subplots()
-        border = PltPath(
-            [(0, 0), (width, 0), (width, height), (0, height), (0, 0)],
-            [
-                PltPath.MOVETO,
-                PltPath.LINETO,
-                PltPath.LINETO,
-                PltPath.LINETO,
-                PltPath.CLOSEPOLY,
-            ],
-        )
-        ax.add_patch(patches.PathPatch(border, edgecolor="black", facecolor="white"))
-        for i, layer in enumerate(layers):
-            codes = []
-            coords = []
-            for path in layer.paths:
-                codes += [PltPath.MOVETO] + ([PltPath.LINETO] * (len(path) - 1))
-                coords += [(x, height - y) for x, y in path]
-            plt_path = PltPath(coords, codes)
-            ax.add_patch(
-                patches.PathPatch(plt_path, edgecolor=colors[i], fill=False)
-            )
-
-        plt.xlim([-0.5, width + 0.5])
-        plt.ylim([-0.5, height + 0.5])
-        ax.axis("equal")
-        plt.show()
---
+++ axi/renderer.py
@@ -0,0 +1,100 @@
+from __future__ import division
+
+from itertools import chain
+
+from matplotlib import pyplot as plt, patches
+from matplotlib.path import Path as PltPath
+from pyglet import window, gl, app
+from pyglet.graphics import Batch, Group
+
+from axi import Drawing
+
+# colors = [
+#     "blue",
+#     "red",
+#     "darkgreen",
+#     "orange",
+#     "aqua",
+#     "fuchsia",
+#     "lime",
+#     "hotpink",
+# ]
+
+colors = [
+    (0, 0, 255, 255),
+    (255, 0, 0, 255),
+    (0, 82, 33, 255),
+    (255, 123, 0, 255),
+    (8, 142, 149, 255),
+    (255, 0, 255, 255),
+    (158, 253, 56, 255),
+    (206, 81, 113, 255),
+]
+
+
+def batch_drawings(drawings: list[Drawing], width: float, height: float,
+                   dpi: float) -> Batch:
+    assert len(drawings) <= len(colors)
+    batch = Batch()
+    for i, drawing in enumerate(drawings):
+        color = colors[i]
+        for path in drawing.paths:
+            grp = Group()
+            path = [(dpi * x, dpi * (height - y)) for x, y in path]
+            vertices = path[0] + tuple(chain(*path)) + path[-1]
+            batch.add(len(vertices) // 2, gl.GL_LINE_STRIP, grp, ('v2f', vertices),
+                      ('c4B', color * (len(vertices) // 2)))
+    return batch
+
+
+def render_gl(drawings: list[Drawing], width: float, height: float, dpi=128):
+    batch = batch_drawings(drawings, width, height, dpi)
+    win = window.Window(int(width * dpi), int(height * dpi), "plot preview")
+
+    @win.event
+    def on_draw():
+        win.clear()
+        batch.draw()
+
+    app.run()
+
+
+@staticmethod
+def render_matplotlib(layers: list[Drawing], width: float, height: float):
+    colors = [
+        "blue",
+        "red",
+        "darkgreen",
+        "orange",
+        "aqua",
+        "fuchsia",
+        "lime",
+        "hotpink",
+    ]
+    fig, ax = plt.subplots()
+    border = PltPath(
+        [(0, 0), (width, 0), (width, height), (0, height), (0, 0)],
+        [
+            PltPath.MOVETO,
+            PltPath.LINETO,
+            PltPath.LINETO,
+            PltPath.LINETO,
+            PltPath.CLOSEPOLY,
+        ],
+    )
+    ax.add_patch(patches.PathPatch(border, edgecolor="black", facecolor="white"))
+    for i, layer in enumerate(layers):
+        codes = []
+        coords = []
+        for path in layer.paths:
+            codes += [PltPath.MOVETO] + ([PltPath.LINETO] * (len(path) - 1))
+            coords += [(x, height - y) for x, y in path]
+        plt_path = PltPath(coords, codes)
+        ax.add_patch(
+            patches.PathPatch(plt_path, edgecolor=colors[i], fill=False)
+        )
+
+    plt.xlim([-0.5, width + 0.5])
+    plt.ylim([-0.5, height + 0.5])
+    ax.axis("equal")
+    plt.show()
